22dd547c6382acc12bf64768aa3fb974662a42ed
KIIT <adesh.senapati10@gmail.com>
2022-08-04 23:24:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import AmzReview from "./amz-review";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header"><u>REVIEW ANALYZER</u></header>
+      <AmzReview />
+      <footer className="footer">
+            <small><a href='https://www.linkedin.com/in/adesh-senapati-482b90221/'>Linkedin</a>&nbsp;&nbsp;&nbsp;&nbsp; © 2022 Adesh Senapati. All rights reserved. &nbsp;&nbsp;&nbsp;&nbsp; <a href='https://github.com/AdeshSenapati'>Github</a> </small>
+      </footer>
     </div>
   );
 }
---
+++ src/amz-review.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import * as ReactBootstrap from 'react-bootstrap';
+
+export default function AmzReview(){
+    const [amzInputs, setamzInputs] = React.useState('');
+    const [flipInputs, setflipInputs] = React.useState('');
+    const [amzReviewData, setamzReviewData] = React.useState([]);
+    const [flipReviewData, setflipReviewData] = React.useState([]);
+    const [isShown, setIsShown] = React.useState('');
+    const [isLoading, setLoading] = React.useState(true);
+
+    function toggleAmz(){
+        setIsShown('showAmz')
+    }
+    function toggleFlip(){
+        setIsShown('showFlip')
+    }
+    
+
+    const handleAmzChange = (event) => {
+        setamzInputs(event.target.value)
+      }
+    
+    const HandleAmzSubmit = () => {
+        setLoading(false);
+        async function getData() {
+            const res = await fetch(`https://review-api10.herokuapp.com/get-amz-results?item=${amzInputs}`)
+            const data = await res.json()
+            setamzReviewData(data);
+            console.log(data);
+            setLoading(true);
+        }
+        getData()
+    }
+
+    const handleFlipChange = (event) => {
+        setflipInputs(event.target.value)
+      }
+    
+    const HandleFlipSubmit = () => {
+        setLoading(false);
+        async function getData() {
+            const res = await fetch(`https://review-api10.herokuapp.com/get-flp-results?item=${flipInputs}`)
+            const data = await res.json()
+            setflipReviewData(data);
+            console.log(data);
+            setLoading(true);
+        }
+        getData()
+    }
+
+
+    return(
+        <div className="review">
+            <div className="buttons">
+                <button className="button-49" onClick={toggleAmz}>AMAZON</button>
+                <button className="button-49" onClick={toggleFlip}>FLIPKART</button>
+            </div>
+            {isShown === 'showAmz' ?
+                <div className="amazon"> 
+                    <div className="amazon-sent">
+                        <input type="text" onChange={handleAmzChange} className="inputbox" placeholder='Enter amazon url' />
+                            &nbsp;
+                        <button onClick={HandleAmzSubmit} className="button-33">SEND</button>
+                    </div>
+                    {isLoading ? <div className="amz-results">
+                        <br />
+                        <p>{amzReviewData.data}</p>
+                        <p>{amzReviewData.positive}</p>
+                        <p>{amzReviewData.negative}</p>
+                        <p>{amzReviewData.neutral}</p>
+                    </div>: <div className="spinner"><ReactBootstrap.Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </ReactBootstrap.Spinner></div>}
+                </div>
+            : null}
+            {isShown === 'showFlip' ?
+                <div className="flipkart"> 
+                    <input type="text" onChange={handleFlipChange} className="inputbox" placeholder='Enter flipkart url' />
+                    &nbsp;
+                    <button onClick={HandleFlipSubmit} className="button-33">SEND</button>
+                    {isLoading ? <div className="flip-results">
+                        <br />
+                        <p>{flipReviewData.data}</p>
+                        <p>{flipReviewData.positive}</p>
+                        <p>{flipReviewData.negative}</p>
+                        <p>{flipReviewData.neutral}</p>
+                    </div>: <div className="spinner"><ReactBootstrap.Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </ReactBootstrap.Spinner></div>}
+                </div>
+            : null}
+            
+        </div>
+    );
+}
